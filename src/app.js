a64a1ffe95ac65bf3532bb4a43c8cb6c57c53d64
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-05-23 23:11:42 +0000

    another commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "superstar"];
+  let noun = ["beyonce", "rihanna"];
+
+  var domainGenerator = (pronoun, adj, noun) => {
+    for (var i = 0; i < pronoun.length; i++) {
+      for (var j = 0; j < adj.length; j++) {
+        for (var r = 0; r < noun.length; r++) {
+          console.log(pronoun[i] + adj[j] + noun[r] + ".com");
+        }
+      }
+    }
+  };
+  domainGenerator(pronoun, adj, noun);
 };
